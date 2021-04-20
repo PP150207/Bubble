978644506cc61bd985b7431af0656158024758f6
junpeizuno <junpeizuno@gmail.com>
2021-04-20 14:09:58 +0900

    コンテンツ画面を横スクロール可能に

--- resources/views/contents.blade.php
+++ resources/views/contents.blade.php
@@ -1,11 +1,25 @@
-<div>
-	@foreach($contents as $content)
-	<div>
-		<img src="{{ Storage::url($content->file_path) }}" style="width:100%;"/>	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	<title>Document</title>
+</head>
+<body>
+<div >
+	<div class='top-content'>
+		@foreach($contents as $content)
+		<div>
+		<div class='content-list'></div>
+		<img src="{{ Storage::url($content->file_path) }}"  id='item'/>	
+		</div>
+		@endforeach
 	</div>
-	@endforeach
 	<div>
-		<p>{{ $content->title }}</p>
+		<p class='hoge'>{{ $content->title }}</p>
 	</div>
 </div>
 
+</body>
+</html>
--- resources/views/image_list.blade.php
+++ resources/views/image_list.blade.php
@@ -1,11 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	<title>Document</title>
+</head>
+<body>
 <a href="{{ route('upload_form') }}">Upload</a>
 <hr />
 
 @foreach($images as $image)
-<div>
-	<a href='/list/{{$image->id}}'><img src="{{ Storage::url($image->file_path) }}" style="width:100%;"/>
-	<p>{{ $image->title }}</p>
-    </a>
-</div>
+	<div>
+		<a href='/list/{{$image->id}}'><img src="{{ Storage::url($image->file_path) }}" /></a>
+		<p>{{ $image->title }}</p>
+	</div>
 @endforeach
 
+<div>
+
+</div>
+
+</body>
+</html>
