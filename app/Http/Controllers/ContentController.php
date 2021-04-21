ad02423421cd06a5ecff3da3941ddac2259993c1
junpeizuno <junpeizuno@gmail.com>
2021-04-21 15:08:12 +0900

    複数枚画像がある時も、storage、db全てから画像を削除

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -5,7 +5,6 @@ use Illuminate\Http\Request;
 use App\Models\Edit;
 use Illuminate\Support\Facades\Storage;
 
-
 class ContentController extends Controller
 {
     function delete(Request $request) {
@@ -14,17 +13,27 @@ class ContentController extends Controller
         
 
         $delete = new Edit;
-        $value = $delete::where('created_at', $delete_create_time)->first();
+        var_dump($delete_create_time);
+        $values = $delete::where('created_at', $delete_create_time)->get();
+        $cnt = count($values, COUNT_RECURSIVE);
 
-        DBからデータを削除
-        $value->delete();
-
-        // 作られた日にちに該当するfaile_pathを読み込んでstorageフォルダから画像を削除
-        $delete_file_path = $value->file_path;
-        var_dump($delete_file_path);
-
-        Storage::delete("public/$delete_file_path");
+        if($cnt <= 1) {
+            $value = $delete::where('created_at', $delete_create_time)->first();
+            $value->delete();
+            $delete_file_path = $value->file_path;
+            Storage::delete("public/$delete_file_path");
+        }elseif($cnt>1){
+            
+            $values = $delete::where('created_at', $delete_create_time)->get();
+            foreach($values as $value) {
+                $delete_file_path = $value->file_path;
+                var_dump($delete_file_path);
+                Storage::delete("public/$delete_file_path");
+                $value->delete();
+            }
+        }
 
         return redirect('/home');
+
     }
 }
